940e484970aeedef24fcfa5eba0fadac238580ef
RobbingDaHood <Christcore1>
2017-07-04 20:29:16 +0200

    First part of backend: Implemented create and get for users and OneTimeEvents

---
+++ lambda-backend/oneTimeEvents/create.js
@@ -0,0 +1,25 @@
+import uuid from 'uuid';
+import * as dynamoDbLib from '../libs/dynamodb-lib';
+import { success, failure } from '../libs/response-lib';
+
+export async function main(event, context, callback) {
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: 'OneTimeEvent',
+    Item: {
+      EventId: uuid.v1(),
+      EventStartDateTime: data.EventStartDateTime,
+      Title: data.Title,
+      Description: data.Description,
+      createdAt: new Date().getTime(),
+    },
+  };
+
+  try {
+    const result = await dynamoDbLib.call('put', params);
+    callback(null, success(params.Item));
+  }
+  catch(e) {
+    callback(null, failure({status: false}));
+  }
+};
