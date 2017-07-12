6cbcac4da6f6affe6a9aae72e279ce8cc77474ac
Yaxuan He <yaxuanhe@hotmail.com>
2017-07-12 21:14:43 +0200

    Added react to eslint config, changes some files accordingly and added Login.js and .css

--- happytohelp-app-client/src/App.js
+++ happytohelp-app-client/src/App.js
@@ -1,43 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import RouteNavItem from './components/RouteNavItem';
-import {
-  withRouter,
-  Link
-} from 'react-router-dom';
-import {
-  Nav,
-  Navbar
-} from 'react-bootstrap';
+import {withRouter, Link} from 'react-router-dom';
+import {Nav, Navbar} from 'react-bootstrap';
 import './App.css';
 import Routes from './Routes';
 
 class App extends Component {
-	handleNavLink = (event) => {
-  event.preventDefault();
-  this.props.history.push(event.currentTarget.getAttribute('href'));
-}
+  handleNavLink = (event) => {
+    event.preventDefault();
+    var testing = "Testing!";
+    this.props.history.push(event.currentTarget.getAttribute('href'));
+  }
 
   render() {
-  return (
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">🙂 Happy to Help</Link>
-          </Navbar.Brand>
-          <Navbar.Toggle />
-        </Navbar.Header>
-        <Navbar.Collapse>
-          <Nav pullRight>
-            <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
-			<RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <Routes />
-    </div>
-  );
-}
+    return (
+      <div className="App container">
+        <Navbar fluid collapseOnSelect>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <Link to="/">🙂 Happy to Help</Link>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullRight>
+              <RouteNavItem onClick={this.handleNavLink} href="/signup">Signup</RouteNavItem>
+              <RouteNavItem onClick={this.handleNavLink} href="/login">Login</RouteNavItem>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <Routes/>
+      </div>
+    );
+  }
 }
 
 export default withRouter(App);
