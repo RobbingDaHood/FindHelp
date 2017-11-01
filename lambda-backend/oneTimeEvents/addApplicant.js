14deddb702c9745cc3871877ed48b3608ab9afa0
RobbingDaHood <Christcore1>
2017-11-01 18:41:52 +0100

    Add applicant. Maybe change name in future.

---
+++ lambda-backend/oneTimeEvents/addApplicant.js
@@ -0,0 +1,30 @@
+import * as dynamoDbLib from '../libs/dynamodb-lib'
+import { success, failure } from '../libs/response-lib'
+
+export async function main (event, context, callback) {
+  var res = event.pathParameters.id.split('&')
+  const params = {
+    TableName: 'OneTimeEvents',
+    // 'Key' defines the partition key and sort key of the item to be updated
+    // - 'userId': User Pool sub of the authenticated user
+    // - 'noteId': path parameter
+    //
+    // Remember: Type need to be a list :)
+    Key: {
+      HashKey: res[0],
+      RangeKey: res[1]
+    },
+    UpdateExpression: 'SET aps = list_append(aps, :testing)',
+    ExpressionAttributeValues: {
+      ':testing': [event.requestContext.authorizer.claims.sub]
+    },
+    ReturnValues: 'ALL_NEW'
+  }
+
+  try {
+    await dynamoDbLib.call('update', params)
+    callback(null, success({status: true}))
+  } catch (e) {
+    callback(null, failure(e))
+  }
+};
