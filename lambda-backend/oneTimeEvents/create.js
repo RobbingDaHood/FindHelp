671cf79a8b43651b07bd1160e4a4fedd64e5734b
RobbingDaHood <Christcore1>
2017-11-18 17:05:39 +0100

    Added JUnit tests. now I can run "npm test".

--- lambda-backend/oneTimeEvents/create.js
+++ lambda-backend/oneTimeEvents/create.js
@@ -3,16 +3,16 @@ import * as dynamoDbLib from '../libs/dynamodb-lib'
 import {success, failure} from '../libs/response-lib'
 
 export async function main (event, context, callback) {
-  const data = JSON.parse(event.body)
+  const body = JSON.parse(event.body)
   const params = {
     TableName: 'OneTimeEvents',
     Item: {
-      HashKey: data.GeoPointHash,
+      HashKey: body.GeoPointHash,
       RangeKey: uuid.v1(),
       OwnerId: event.requestContext.authorizer.claims.sub,
-      EventStartDateTime: data.EventStartDateTime,
-      Title: data.Title,
-      Description: data.Description,
+      EventStartDateTime: body.EventStartDateTime,
+      Title: body.Title,
+      Description: body.Description,
       Applicants: [],
       CreatedAt: new Date().getTime(),
       UpdatedAt: new Date().getTime()
